d8aa9645e1fc40b1f39355230d201ec314d2284b
Shan.Sun <shan.sun@noaa.gov>
2020-10-31 16:17:23 +0000

    Saving tiice in the restart file, regardless of grid_frac value.

--- io/FV3GFS_io.F90
+++ io/FV3GFS_io.F90
@@ -955,11 +955,11 @@ module FV3GFS_io_mod
     endif
 #endif
       !--- register the 3D fields
-    if (Model%frac_grid) then
+!   if (Model%frac_grid) then
       sfc_name3(0) = 'tiice'
       var3_p => sfc_var3ice(:,:,:)
       id_restart = register_restart_field(Sfc_restart, fn_srf, sfc_name3(0), var3_p, domain=fv_domain, mandatory=.false.)
-    end if
+!   end if
  
     do num = 1,nvar_s3
       var3_p => sfc_var3(:,:,:,num)
@@ -1329,7 +1329,7 @@ module FV3GFS_io_mod
       endif
 
       if (sfc_var2(i,j,nvar_s2m) < -9990.0_r8) then
-        if (Model%me == Model%master ) call mpp_error(NOTE, 'gfs_driver::surface_props_input - computing zorli')
+        if (Model%me == Model%master ) call mpp_error(NOTE, 'gfs_driver::surface_props_input - computing zorlw')
 !$omp parallel do default(shared) private(nb, ix)
         do nb = 1, Atm_block%nblks
           do ix = 1, Atm_block%blksz(nb)
@@ -1978,11 +1978,11 @@ module FV3GFS_io_mod
 #endif
 
       !--- register the 3D fields
-      if (Model%frac_grid) then
+!      if (Model%frac_grid) then
         sfc_name3(0) = 'tiice'
         var3_p => sfc_var3ice(:,:,:)
         id_restart = register_restart_field(Sfc_restart, fn_srf, sfc_name3(0), var3_p, domain=fv_domain)
-      endif
+!      endif
 
       do num = 1,nvar3
         var3_p => sfc_var3(:,:,:,num)
